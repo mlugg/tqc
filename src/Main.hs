7831320fcc825e81d3381641cc303f9b3e15a6c8
mlugg <mlugg@mlugg.co.uk>
2020-11-03 01:30:32 +0000

    Update Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import qualified Data.Text.IO as TIO
+import Text.Megaparsec
+import QntSyn
+import QntSyn.Parse
+import QntSyn.Infer
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  t <- TIO.getContents
+  case parse expr "<stdin expr>" t of
+    Left err -> putStrLn $ errorBundlePretty err
+    Right e  -> case doInfer e of
+      Left err -> print err
+      Right t  -> TIO.putStrLn $ pPrintTypeScheme t
